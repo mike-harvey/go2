a32a0712f983f93912ffd60780ebd1d7b721305a
mike-harvey <m8harvey@yahoo.com>
2025-02-12 09:37:10 +0000

    string edge detection is working

--- edge.c
+++ edge.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef struct part_path_t
+{
+    int path_direction_in;
+    int path_direction_out;
+    bool path_visits_centre;
+    bool path_is_active;
+}
+part_path_t;
 
 typedef struct edgeDirectionConverter_t
 {
     // These indicate if an edge exists between the direction and the next
     int string_edge_in[8];
     int string_edge_out[8];
-    int region_edge_in[8];
-    int region_edge_out[8];
 
     // these map incoming dirctions to outgoing directions
-    int string_edge_map[8];
-    int region_edge_map[8];
+    int string_edge_part_path_count;
+    part_path_t string_edge_part_paths[4];
+
+    //int region_edge_in[8];
+    //int region_edge_out[8];
+    //path_t region_edge_paths[4];
 }
 edgeDirectionConverter_t;
 
@@ -24,6 +37,7 @@ int wrap_direction(int d)
     {
         d += 8;
     }
+    assert(0 <= d && d < 8);
     return d;
 }
 
@@ -45,8 +59,18 @@ int main(int argc, char **argv)
 	{
             edgeConverter[i].string_edge_in[j] = 0;
             edgeConverter[i].string_edge_out[j] = 0;
-            edgeConverter[i].region_edge_in[j] = 0;
-            edgeConverter[i].region_edge_out[j] = 0;
+            //edgeConverter[i].region_edge_in[j] = 0;
+            //edgeConverter[i].region_edge_out[j] = 0;
+	}
+
+	edgeConverter[i].string_edge_part_path_count = 0;
+
+	for(int j = 0; j < 4; j++)
+	{
+	    edgeConverter[i].string_edge_part_paths[j].path_is_active = false;
+	    edgeConverter[i].string_edge_part_paths[j].path_visits_centre = false;
+	    edgeConverter[i].string_edge_part_paths[j].path_direction_in = 0;
+	    edgeConverter[i].string_edge_part_paths[j].path_direction_out = 0;
 	}
 
 	for(int j = 0; j < 8; j++)
@@ -65,12 +89,52 @@ int main(int argc, char **argv)
 	}
     }
 
+    // Figure out the string edge paths
+    for(int i = 0; i < 256; i++)
+    {
+        for(int j=0; j < 8; j++)
+	{
+	    if(edgeConverter[i].string_edge_out[j])
+	    {
+	        for(int k = 1; k <= 8; k++)
+		{
+		    if(edgeConverter[i].string_edge_in[wrap_direction(j + k)])
+		    {
+		        // Found an edge - in and out
+                        int path = edgeConverter[i].string_edge_part_path_count++;
+
+	                edgeConverter[i].string_edge_part_paths[path].path_is_active = true;
+			edgeConverter[i].string_edge_part_paths[path].path_visits_centre = true;
+			edgeConverter[i].string_edge_part_paths[path].path_direction_in = wrap_direction(j + k);
+			edgeConverter[i].string_edge_part_paths[path].path_direction_out = j;
+
+			if(j % 2 == 0 && k == 2)
+			{
+			    edgeConverter[i].string_edge_part_paths[path].path_visits_centre = false;
+			}
+			break;
+		    }
+		}
+	    }
+	}
+    }
+
     // dump 'em
     for(int i = 0; i < 256; i++)
     {
         printf("\n%c %c %c\n", i & (1 << 3) ? 'X' : '.', i & (1 << 2) ? 'X' : '.', i & (1 << 1) ? 'X' : '.');
         printf("%c . %c\n", i & (1 << 4) ? 'X' : '.', i & 1 ? 'X' : '.');
         printf("%c %c %c\n\n", i & (1 << 5) ? 'X' : '.', i & (1 << 6) ? 'X' : '.', i & (1 << 7) ? 'X' : '.');
+
+	for(int j = 0; j < edgeConverter[i].string_edge_part_path_count; j++)
+	{
+	    printf("path: active ? %c, visits_centre ? %c, in %d, out %d\n",
+		   edgeConverter[i].string_edge_part_paths[j].path_is_active ? 'T' : 'F',
+		   edgeConverter[i].string_edge_part_paths[j].path_visits_centre ? 'T' : 'F',
+		   edgeConverter[i].string_edge_part_paths[j].path_direction_in,
+		   edgeConverter[i].string_edge_part_paths[j].path_direction_out);
+	}
+#if 0
 	for(int j = 0; j < 8; j++)
 	{
 	    if(edgeConverter[i].string_edge_in[j])
@@ -91,5 +155,6 @@ int main(int argc, char **argv)
 	        printf("\n");
 	    }
 	}
+#endif
     }
 }
